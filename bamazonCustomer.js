5044f58008583bb68c55355e860fa959bf86883c
jharnois444 <jharnois444@gmail.com>
2018-05-11 19:26:40 -0400

    alternate method

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,9 @@
+// Dependencies
 const mysql = require('mysql'),
       inquirer = require('inquirer'),
       Table = require('cli-table'),
 
+    // Create MySQL connection
      connection = mysql.createConnection({
         host: 'localhost',
         port: 3306,
@@ -10,74 +12,138 @@ const mysql = require('mysql'),
         database: 'bamazon_db'
       });
 
+      // Initialize MySQL connecion
       connection.connect((err) => {
         if (err) throw err;
-        console.log('Connected!');
-        displayProducts();
+        // console.log('Connected!');
+        // displayProducts();
+        makeTable();
       });
 
+// Print items in DB to screen
+var makeTable = function(){
+    connection.query("SELECT * FROM products", function(err,res){
+        for(var i=0; i<res.length; i++){
+            console.log(res[i].item_id + ' || ' + res[i].product_name + ' || ' + res[i].department_name + ' || ' + res[i].price + ' || ' + res[i].stock_quantity + '\n');
+        }
+    promptCustomer(res);
+    })
+}
 
+// Ask customer which item they would like to buy
+var promptCustomer = function(res) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'choice',
+            message: 'Which item number would you like to purchase? [Quit with Q]'
+        }
+    ]).then(function(answer) {
+        var correct = false;
+        if(answer.choice.toUpperCase() == 'Q') {
+            process.exit();
+        }
+        for(var i = 0; i < res.length; i++) {
+            if(res[i].item_id == answer.choice) {
+                correct = true;
+                var product = answer.choice;
+                var id = i;
 
-
-//connect to the mysql database and pull the information from the Products database to display to the user
-function displayProducts() {
-	
-	var sql = 'SELECT item_id, product_name, price, stock_quantity FROM products'
-
-	connection.query(sql, function(err, result){
-
-		if(err) console.log(err)
-
-		//creates a table for the information from the mysql database to be placed
-		console.log('>>>>>>Products Available for Purchase<<<<<<')
-		var table = new Table({
-			head: ['Item Id#', 'Product Name', 'Price', 'stock_quantity'],
-			//chars: chars,
-			colAligns: [null, null, 'right'],
-			style: {
-				head: ['blue'],
-				compact: false
-			}
-		})
-
-		//loops through each item in the mysql database and pushes that information into a new row in the table
-		
-		for(var i = 0; i < result.length; i++){
-			table.push([result[i].item_id, result[i].product_name, result[i].price, result[i].stock_quantity])
-		}
-		//show the product info in tabular form
-
-		console.log(table.toString())
-		
-		//determine what customer wants to do
-		
-		itemIdToBuy(result)
-	})
+                // Ask customer how many they would like to buy
+                inquirer.prompt(
+                    {
+                        type: 'input',
+                        name: 'quant',
+                        message: 'How many would you like to buy?',
+                        validate: function(value) {
+                            if(NaN(value) == false) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
+                    }).then(function(answer) {
+                        if((res[id].stock_quantity-answer.quant) > 0) {
+                            connection.query('UPDATE products SET stock_quantity = ' + (res[id].stock_quantity - answer.quant) + 'WHERE item_id = ' + product + "'", function(err,res2) {
+                                console.log('Product Bought!');
+                                makeTable();
+                            })
+                        } else {
+                            console.log('Not a valid selection!');
+                            promptCustomer(res);
+                        }
+                    })
+            }
+        }
+    if(i == res.length && correct == false) {
+        console.log('Not a valid selection!');
+        promptCustomer(res);
+    }
+    })
 }
 
 
 
-// Ask customer the ID of the product they would like to buy.
- itemIdToBuy = function(result) {
-    inquirer
-    .prompt({
-      name: "userItem",
-      type: "input",
-      message: "Which item would you like to purchase?"
-    })
-    .then(function(answer) {
-        for(i=0; i<result.length; I++){
-            if (result[i].item == answer.userItem)
-            console.log(error)
-        }
-      // based on their answer, either call the bid or the post functions
-      if (answer.userItem <= 10) {
-        postAuction();
-      }
-      else {
-        bidAuction();
-      }
-    });
+//connect to the mysql database and pull the information from the Products database to display to the user
+// function displayProducts() {
+	
+// 	var sql = 'SELECT item_id, product_name, price, stock_quantity FROM products'
+
+// 	connection.query(sql, function(err, result){
+
+// 		if(err) console.log(err)
+
+// 		//creates a table for the information from the mysql database to be placed
+// 		console.log('>>>>>>Products Available for Purchase<<<<<<')
+// 		var table = new Table({
+// 			head: ['Item Id#', 'Product Name', 'Price', 'stock_quantity'],
+// 			//chars: chars,
+// 			colAligns: [null, null, 'right'],
+// 			style: {
+// 				head: ['blue'],
+// 				compact: false
+// 			}
+// 		})
+
+// 		//loops through each item in the mysql database and pushes that information into a new row in the table
+		
+// 		for(var i = 0; i < result.length; i++){
+// 			table.push([result[i].item_id, result[i].product_name, result[i].price, result[i].stock_quantity])
+// 		}
+// 		//show the product info in tabular form
+
+// 		console.log(table.toString())
+		
+// 		//determine what customer wants to do
+		
+// 		itemIdToBuy(result)
+// 	})
+// };
+
+
+
+// // Ask customer the ID of the product they would like to buy.
+//  itemIdToBuy = function(result) {
+//     inquirer
+//     .prompt({
+//       name: "userItem",
+//       type: "input",
+//       message: "Which item would you like to purchase?"
+//     })
+//     .then(function(answer) {
+//         for(i=0; i<result.length; I++){
+//             if (result[i].item == answer.userItem)
+//             console.log(error)
+//         }
+//       // based on their answer, either call the bid or the post functions
+//       if (answer.userItem <= 10) {
+//         postAuction();
+//       }
+//       else {
+//         bidAuction();
+//       }
+//     });
+
 
 
 
